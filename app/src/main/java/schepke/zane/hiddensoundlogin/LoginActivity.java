7af00a115f90212a6ec70c4fd2e6de4fc3f0d4db
Zane <zanecschepke@gmail.com>
2017-02-01 14:34:26 -0500

    Initial Commit

---
+++ app/src/main/java/schepke/zane/hiddensoundlogin/LoginActivity.java
@@ -0,0 +1,27 @@
+package schepke.zane.hiddensoundlogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+    public void register(View v){
+        startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+    }
+
+
+
+}
